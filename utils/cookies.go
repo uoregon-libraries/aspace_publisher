7b61028c4fefedeb2252222676a26c51b58f9619
lsat12357 <lsato@uoregon.edu>
2023-10-17 09:35:07 -0700

    make expirable cookie

--- utils/cookies.go
+++ utils/cookies.go
@@ -6,12 +6,20 @@ import(
   "github.com/labstack/echo/v4"
 )
 
-func WriteCookie(c echo.Context, name string, value string) {
+func WriteCookie(c echo.Context, expires int, name string, value string) {
   cookie := new(http.Cookie)
   cookie.Name = name
   cookie.Value = value
-  cookie.Expires = time.Now().Add(5 * time.Hour)
+  cookie.Expires = time.Now().Add(time.Duration(expires) * time.Hour)
   c.SetCookie(cookie)
+}
+
+func FetchExpirableCookie(c echo.Context, name string) (string, error){
+  if Expired(c, name) { return "", nil }
+  cookie, err := c.Cookie(name)
+  if err != nil { return "", err }
+
+  return cookie.Value, nil
 }
 
 func FetchCookieVal(c echo.Context, name string) (string, error) {
@@ -19,4 +27,12 @@ func FetchCookieVal(c echo.Context, name string) (string, error) {
   if err != nil { return "", err }
 
   return cookie.Value, nil
+}
+
+func Expired(c echo.Context, name string) (bool) {
+  cookie, err := c.Cookie(name)
+  if err != nil { return true }
+  if cookie.Expires.After(time.Now()) {
+    return false
+  } else { return true }
 }
