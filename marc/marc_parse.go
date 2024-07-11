7d96f9fb3fe0df0f701fb70486d95a65230e2574
lsat12357 <lsato@uoregon.edu>
2024-07-11 09:48:11 -0700

    trim oclc

--- marc/marc_parse.go
+++ marc/marc_parse.go
@@ -4,14 +4,22 @@ import (
   "github.com/beevik/etree"
   "log"
   "errors"
+  "strings"
 )
 
 func ExtractOclc(marc string)(string, error){
   marc_xml := etree.NewDocument()
   err := marc_xml.ReadFromString(marc)
   if err != nil { log.Println(err); return "", errors.New("Unable to read XML response from OCLC.") }
-  oclc := marc_xml.FindElement("//controlfield[@tag='001']").Text()
+  oclc_val := marc_xml.FindElement("//controlfield[@tag='001']").Text()
+  oclc := cleanOclc(oclc_val)
   return oclc, nil
+}
+
+func cleanOclc(oclc_id string)string {
+  return strings.TrimFunc(oclc_id, func(r rune) bool{
+    return !unicode.IsNumber(r)
+  })
 }
 
 func StripOuterTags(marc string)(string, error){
