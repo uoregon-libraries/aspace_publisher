ce62b654c3752f7a4a2cbc1327366d9b73af8659
lsat12357 <lsato@uoregon.edu>
2025-08-27 13:02:44 -0700

    report errors

--- alma/construct.go
+++ alma/construct.go
@@ -11,22 +11,27 @@ import (
   "strings"
 )
 
-func ConstructBib(marc_string string)string{
+func ConstructBib(marc_string string)(string, error){
   marc_stripped, err := marc.StripOuterTags(marc_string)
-  if err != nil { log.Println(err); return "" }
-  return "<bib>" + marc_stripped + "</bib>"
+  if err != nil { return "", err }
+  return "<bib>" + marc_stripped + "</bib>", nil
 }
 
-func ConstructHolding(marc_string string)string{
+func ConstructHolding(marc_string string)(string, error){
   marc_xml, err := ParseMarc(marc_string)
-  if err != nil { log.Println(err); return "" }
-  call_num := ExtractCall(marc_xml)
-  link := BuildFindingLink(marc_xml)
-  fixed := ExtractFixed(marc_xml)
+  if err != nil { return "", err }
+  call_num, err := ExtractCall(marc_xml)
+  if err != nil { return "", err }
+  link, err := BuildFindingLink(marc_xml)
+  if err != nil { return "", err }
+  fixed, err := ExtractFixed(marc_xml)
+  if err != nil { return "", err }
   var h = Holding{}
   h.Suppress = false
   var rec Record
-  rec.Leader = ExtractLeader(marc_xml)
+  rec.Leader, err = ExtractLeader(marc_xml)
+  if err != nil { return "", err }
+
   rec.Cfields = []Controlfield{ Controlfield{Tag:"008", Value: fixed} }
   sfb := Subfield{Code:"b", Value:"Special Collections"}
   sfc := Subfield{Code:"c", Value: "spmanus"}
@@ -39,11 +44,11 @@ func ConstructHolding(marc_string string)string{
   rec.Dfields = []Datafield{ df852, df866 }
   h.Rec = rec
   output, err := xml.MarshalIndent(h, "  ", "    ")
-  if err != nil { log.Println(err); return "" }
-  return string(output)
+  if err != nil { log.Println(err); return "", errors.New("unable to contruct holding json") }
+  return string(output), nil
 }
 
-func ConstructItem(item_id string, holding_id string, tc_data map[string]string)string{
+func ConstructItem(item_id string, holding_id string, tc_data map[string]string)(string, error){
   var item = Item{}
   item.Holding_data = HoldingData{ Holding_id: holding_id }
   var idata = ItemData{}
@@ -56,8 +61,8 @@ func ConstructItem(item_id string, holding_id string, tc_data map[string]string)
   idata.Physical_material_type = Value{ Val: "MANUSCRIPT" }
   item.Item_data = idata
   data, err := json.Marshal(item)
-  if err != nil { log.Println(err); return "" }
-  return string(data)
+  if err != nil { log.Println(err); return "", errors.New("unable to construct item json") }
+  return string(data), nil
 }
 
 func policy(_type string)string{
@@ -104,30 +109,35 @@ func ParseMarc(marc_string string)(*etree.Document, error){
 //856->866 which is not on the LOC reference
 //marc uses z for the display message, u for the url
 //866 is a mashup, z, value is a link
-func BuildFindingLink(marc_xml *etree.Document)string{
-  url := marc_xml.FindElement("//datafield[@tag='856']/subfield[@code='u']").Text()
-  text := marc_xml.FindElement("//datafield[@tag='856']/subfield[@code='z']").Text()
-  link := fmt.Sprintf("<a href='%s' target='_blank'>%s</a>", url, text)
-  if text == "Notice of Interest in Unprocessed Collections" {
+func BuildFindingLink(marc_xml *etree.Document)(string, error){
+  url := marc_xml.FindElement("//datafield[@tag='856']/subfield[@code='u']")
+  if url == nil { return "", errors.New("unable to extract 856") }
+  text := marc_xml.FindElement("//datafield[@tag='856']/subfield[@code='z']")
+  if text == nil { return "", errors.New("unable to extract 856") }
+  link := fmt.Sprintf("<a href='%s' target='_blank'>%s</a>", url.Text(), text.Text())
+  if text.Text() == "Notice of Interest in Unprocessed Collections" {
     link = "UNARRANGED COLLECTION UNAVAILABLE FOR USE. Inquiries regarding these materials should be submitted via the " + link
   }
-  return link
+  return link, nil
 }
 
 //leader
-func ExtractLeader(marc_xml *etree.Document) string{
-  leader := marc_xml.FindElement("//leader").Text()
-  return leader
+func ExtractLeader(marc_xml *etree.Document)(string, error){
+  leader := marc_xml.FindElement("//leader")
+  if leader == nil { return "", errors.New("unable to extract leader") }
+  return leader.Text(), nil
 }
 
 //df 099 sf a -> df 852 sf h
-func ExtractCall(marc_xml *etree.Document)string{
-  call := marc_xml.FindElement("//datafield[@tag='099']/subfield[@code='a']").Text()
-  return call
+func ExtractCall(marc_xml *etree.Document)(string, error){
+  call := marc_xml.FindElement("//datafield[@tag='099']/subfield[@code='a']")
+  if call == nil { return "", errors.New("unable to extract 099") }
+  return call.Text(), nil
 }
 
 //CF 008 -> 008
-func ExtractFixed(marc_xml *etree.Document)string{
-  fixed := marc_xml.FindElement("//controlfield[@tag='008']").Text()
-  return fixed
+func ExtractFixed(marc_xml *etree.Document)(string, error){
+  fixed := marc_xml.FindElement("//controlfield[@tag='008']")
+  if fixed == nil { return "", errors.New("unable to extract 008") }
+  return fixed.Text(), nil
 }
