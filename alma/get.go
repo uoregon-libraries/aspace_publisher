1ffca634415ca624e1cd047b3321affe3d87a4f7
lsat12357 <lsato@uoregon.edu>
2026-03-06 14:42:19 -0800

    parse alma errors from get

--- alma/get.go
+++ alma/get.go
@@ -9,6 +9,8 @@ import(
   "strings"
   "os"
   "aspace_publisher/connect"
+  "github.com/tidwall/gjson"
+  "encoding/xml"
 )
 
 //url /almaws/v1/conf/sets/<setid>/members
@@ -35,7 +37,39 @@ func Get(url string, params []string, accept string)([]byte, error){
   defer response.Body.Close()
   body, err := io.ReadAll(response.Body)
   if err != nil { log.Println(err); return nil, errors.New("unable to read response from alma") }
-  if response.StatusCode != 200 { return body, errors.New("alma errors") }
-
+  if response.StatusCode != 200 {
+    if accept == "application/json" {
+      return body, errors.New(ExtractJsonError(body))
+    } else {
+      return body, errors.New(ExtractXmlError(body))
+    }
+  }
   return body, nil
+}
+
+
+func ExtractJsonError(body []byte)string{
+  return gjson.GetBytes(body, "errorList.error.0.errorMessage").String()
+}
+
+func ExtractXmlError(body []byte)string{
+  var wr WebServiceResult
+  err := xml.Unmarshal(body, &wr)
+  if err != nil { return err.Error() }
+  return wr.ErrorList.AlmaError[0].ErrorMess
+}
+
+type WebServiceResult struct{
+  XMLName xml.Name `xml:"web_service_result"`
+  ErrorsExist bool `xml:"errorsExist"`
+  ErrorList ErrorList `xml:"errorList"`
+}
+type ErrorList struct{
+  XMLName xml.Name `xml:"errorList"`
+  AlmaError []AlmaError `xml:"error"`
+}
+type AlmaError struct {
+  XMLName xml.Name `xml:"error"`
+  ErrorCode string `xml:"errorCode"`
+  ErrorMess string `xml:"errorMessage"`
 }
