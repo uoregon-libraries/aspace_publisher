c8f686e9bb43f72d3ef4ea674869c46d52e544a6
lsat12357 <lsato@uoregon.edu>
2026-09-04 14:11:14 -0700

    check verbose in dumping methods

--- as/aspace_ead.go
+++ as/aspace_ead.go
@@ -4,14 +4,14 @@ import(
   "fmt"
   "time"
   "net/http"
-  "net/http/httputil"
   "errors"
   "io"
   "os"
   "log/slog"
+  "aspace_publisher/connect"
 )
 
-func AcquireEad(sessionid string, repo_id string, resource_id string, verbose string) (string, error){
+func AcquireEad(sessionid string, repo_id string, resource_id string) (string, error){
   base_url := os.Getenv( "ASPACE_URL")
   url := base_url + fmt.Sprintf("repositories/%s/resource_descriptions/%s.xml?include_unpublished=%s&include_daos=%s&numbered_cs=%s&ead3=%s", repo_id, resource_id, "False", "True", "True", "False")
   req, err := http.NewRequest("GET", url, nil)
@@ -21,19 +21,15 @@ func AcquireEad(sessionid string, repo_id string, resource_id string, verbose st
   req.Header.Set("Accept", "*/*")
   req.Header.Set("User-Agent", "curl/7.61.1")
 
+  connect.RequestDump(req)
+
   client := &http.Client{
     Timeout: time.Second * 90,
   }
-  if verbose == "true" {
-    reqdump, err := httputil.DumpRequest(req, true)
-    if err != nil { slog.Error(err.Error()) } else { slog.Info(fmt.Sprintf("REQUEST:\n%s", string(reqdump))) }
-  }
+
   response, err := client.Do(req); if err != nil { slog.Error(err.Error()); return "", err }
   defer response.Body.Close()
-  if verbose == "true" {
-    respdump, err := httputil.DumpResponse(response, true)
-    if err != nil { slog.Error(err.Error()) } else { slog.Info(fmt.Sprintf("RESPONSE:\n%s", string(respdump))) }
-  }
+  connect.ResponseDump(response) //check response only after err check
 
   body, err := io.ReadAll(response.Body); if err != nil { slog.Error(err.Error()); return "", err }
   if response.StatusCode != 200 {
--- as/aspace_export.go
+++ as/aspace_export.go
@@ -14,7 +14,6 @@ import(
 func AcquireMarc(sessionid string, repo_id string, resource_id string, published string) (string, error){
   include := "false"
   if published == "false" { include = "true" }
-  verbose := os.Getenv("VERBOSE")
   base_url := os.Getenv("ASPACE_URL")
   url := base_url + fmt.Sprintf("repositories/%s/resources/marc21/%s.xml?include_unpublished_marc=%s", repo_id, resource_id, include)
   req, err := http.NewRequest("GET", url, nil)
@@ -24,7 +23,7 @@ func AcquireMarc(sessionid string, repo_id string, resource_id string, published
   req.Header.Set("Accept", "*/*")
   req.Header.Set("User-Agent", "curl/7.61.1")
 
-  connect.RequestDump(verbose, req)
+  connect.RequestDump(req)
 
   client := &http.Client{
     Timeout: time.Second * 60,
@@ -32,7 +31,7 @@ func AcquireMarc(sessionid string, repo_id string, resource_id string, published
   response, err := client.Do(req)
   if err != nil { slog.Error(err.Error()); return "", errors.New("unable to complete request to archivesspace") }
   defer response.Body.Close()
-  connect.ResponseDump(verbose, response) //check response only after err check
+  connect.ResponseDump(response) //check response only after err check
   body, err := io.ReadAll(response.Body)
   if err != nil { slog.Error(err.Error()); return "", errors.New("unable to read response from archivesspace") }
   if response.StatusCode != 200 { return string(body), errors.New("aspace error exporting MARC") }
@@ -41,7 +40,7 @@ func AcquireMarc(sessionid string, repo_id string, resource_id string, published
 
 func AcquireJson(sessionid string, repo_id string, record_id string) ([]byte, error){
   base_url := os.Getenv( "ASPACE_URL")
-  verbose := os.Getenv("VERBOSE")
+
   url := base_url + fmt.Sprintf("repositories/%s/%s", repo_id, record_id)
   req, err := http.NewRequest("GET", url, nil)
   if err != nil { slog.Error(err.Error()); return nil, errors.New("unable to create http request") }
@@ -49,14 +48,14 @@ func AcquireJson(sessionid string, repo_id string, record_id string) ([]byte, er
   req.Header.Set("X-ArchivesSpace-Session", sessionid)
   req.Header.Set("Accept", "*/*")
   req.Header.Set("User-Agent", "curl/7.61.1")
-  connect.RequestDump(verbose, req)
+  connect.RequestDump(req)
   client := &http.Client{
     Timeout: time.Second * 60,
   }
   response, err := client.Do(req)
   if err != nil { slog.Error(err.Error()); return nil, errors.New("unable to complete request to archivesspace.") }
   defer response.Body.Close()
-  connect.ResponseDump(verbose, response) //check response only after err check
+  connect.ResponseDump(response) //check response only after err check
   body, err := io.ReadAll(response.Body)
   if err != nil { slog.Error(err.Error()); return nil, errors.New("unable to read response from archivesspace") }
   if response.StatusCode != 200 { return body, errors.New("aspace error exporting record") }
